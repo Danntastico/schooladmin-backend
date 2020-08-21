6d329d664c025b67de08086d033c64d9ea2931a0
daniloWindows <nicolasdpf@gmail.com>
2020-08-21 00:12:57 -0500

    Lazy loading Disabled

--- SchoolAdminAPI/Models/DB.Context.cs
+++ SchoolAdminAPI/Models/DB.Context.cs
@@ -18,6 +18,7 @@ namespace SchoolAdminAPI.Models
         public SchoolAdminAPIC_dbEntities()
             : base("name=SchoolAdminAPIC_dbEntities")
         {
+            this.Configuration.LazyLoadingEnabled = false;
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
